85ba59aa8801386e8953186c77729d0871965bea
TeamD_404 <150044321+TeamD-404@users.noreply.github.com>
2024-07-27 14:56:46 +0530

    Update imposter.py

--- AnonXMusic/plugins/tools/imposter.py
+++ AnonXMusic/plugins/tools/imposter.py
@@ -32,7 +32,7 @@ ROY = [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ",
             url=f"https://t.me/KillerMusicBot?startgroup=true"),
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/roy_editx")
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/itzAsuraa")
     ],
 ]
 
