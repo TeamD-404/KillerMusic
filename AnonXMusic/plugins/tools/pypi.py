7b62f0b9d02226092057b3c6e952f35a10fbb2cb
TeamD_404 <150044321+TeamD-404@users.noreply.github.com>
2024-07-27 14:38:54 +0530

    Update pypi.py

--- AnonXMusic/plugins/tools/pypi.py
+++ AnonXMusic/plugins/tools/pypi.py
@@ -5,7 +5,7 @@ from AnonXMusic import app
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/KillerMusicBot?startgroup=true"),
     ],
 ]
 
